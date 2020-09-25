37a76636bf517c914c731a664b404363ff3ec846
Bipul Karki007 <bipulkarki007@Bipuls-MBP.home>
2020-09-25 01:20:09 -0500

    source code

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "mainwin.h"
+#include "bjd.h"
+#include "bjp.h"
+#include "card.h"
+#include "hand.h"
+#include <iostream>
+
+using namespace std;
+
+#include <gtkmm.h>
+
+
+int main(int argc, char** argv) {
+
+   //Himal
+
+    Gtk::Main app(argc, argv);
+    Mainwin_window mainwin;
+    Gtk::Main::run(mainwin);  
+
+    return 0;
+   
+
+
+   // Create the application
+   auto app = Gtk::Application::create(argc, argv, "edu.uta.cse3310.blackjack");
+
+   // Instance a window
+   Mainwin win;
+
+   // Give control to gtkmm
+   return app->run(win);
+}
---
+++ src/mainwin.h
@@ -0,0 +1,23 @@
+#ifndef MAINWIN_H
+#define MAINWIN_H
+
+#include <gtkmm.h>
+
+class Main_window : public Gtk::Window
+{
+public:
+    Main_window();
+    virtual ~Main_window();
+    //virtual ~BJP_window();
+
+protected:
+    Gtk::Label welcome_title_label;
+    Gtk::Button play_button, rules_button, quit_button;
+    Gtk::VBox vbox;
+
+    void on_play_button_click();
+    void on_rules_button_click();
+    void on_quit_button_click();
+};
+
+#endif
